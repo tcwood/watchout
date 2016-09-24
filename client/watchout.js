c6332d2a5c2a1ffbbe61e79f61b951d895be045b
Hack Reactor Students <accounts@hackreactor.com>
2016-09-24 15:43:27 -0700

    Start creating game end function

--- client/watchout.js
+++ client/watchout.js
@@ -5,14 +5,20 @@ GLOBAL VARS
 =================================
 */
 
-var width = window.innerWidth * .8 + 20;
-var height = window.innerHeight * .6 + 20;
+var width = window.innerWidth;
+var height = window.innerHeight + 45;
 var duration = 2000;
 var numVillians = 1;
+var gameOver = false;
+var addVillianRate = 2;
+var highScore = 0;
+var score = 0;
+
+
 
 var addVillians = function() {
   setInterval(function() {
-    numVillians += 1;
+    numVillians += addVillianRate;
   }, duration);
 };
 
@@ -25,9 +31,39 @@ CREATE BACKGROUND CONTAINER
 */
 var svgContainer = d3.select('.board').append('svg')
                                .attr('id', 'svgContainer')
-                               .style('fill', '#94d31b');
+                               .style('fill', '#ccffff');
 
 
+/*
+=================================
+SCOREBOARD
+=================================
+*/
+
+var increaseScore = setInterval( function() {
+  if (!gameOver) {
+    score += Math.round(numVillians * .2);
+    $('.score').text('Score: ' + score);
+    if (score > highScore) {
+      $('.highScore').text('High Score: ' + score);
+    }
+  }
+}, 20);
+
+
+
+/*
+=================================
+GAME RESET
+=================================
+*/
+var gameReset = function() {
+  score = 0;
+  gameOver = false;
+  numVillians = 1;
+
+
+};
 
 
 /*
@@ -36,8 +72,11 @@ VILLIAN FUNCTIONS
 =================================
 */
 
-var updateData = function(n) {
+var updateData = function(n, trigger) {
   var data = [];
+  if (trigger) {       //empty array if game over
+    return data;
+  }
 
   for (var i = 0; i < n; i++) {
     var tempObj = {};
@@ -49,19 +88,16 @@ var updateData = function(n) {
 
     data.push(tempObj);
   }
-
   return data;
 };
 
 
 var update = function() {
-  var villianObjects = updateData(numVillians);
+  var villianObjects = updateData(numVillians, gameOver);
 
   var villians = svgContainer.selectAll('circle')
                   .data(villianObjects, d => d.id);
 
-                  
-  
   // ENTER
   villians.enter().append('circle')
                   .attr('cx', function(d) {
@@ -69,7 +105,6 @@ var update = function() {
                     var hero = d3.select('.hero').attr('cx');
             
                     if (Math.abs(curr - hero) < heroRadius + villianRadius + 10) {
-                      console.log("too close")
                       return d.xPos + (heroRadius + villianRadius) * 2 + 50;
                     } else {
                       return d.xPos;
@@ -79,7 +114,6 @@ var update = function() {
                   .attr('r', villianRadius)
                   .attr('fill', villianColor)
                   .classed('villian', true); 
-
 
   // UPDATE
   villians
@@ -105,6 +139,8 @@ var update = function() {
               return interpolatorY(t);
             };
           });
+
+
 };
 
 
@@ -135,8 +171,6 @@ var drag = d3.behavior
 
 
 var collision = function() {
-
-
   var hX = d3.select('.hero').attr('cx');
 
   var hY = d3.select('.hero').attr('cy');
@@ -151,8 +185,9 @@ var collision = function() {
     centersDistance = Math.pow((Math.pow(xDiff, 2) + Math.pow(yDiff, 2)), .5);
     // console.log(centersDistance);
     if (centersDistance < heroRadius + villianRadius) {
-      console.log('COLLISION!!!!');
-
+      
+      d3.select('.hero').attr('fill', 'red');
+      gameOver = true;
     }
 
   });
@@ -171,56 +206,27 @@ var heroRadius = 30;
 var heroColor = '#94d31b';
 
 
-var hero = svgContainer.selectAll('hero')
-                       .data([{x: 1, y: 1, xPos: width / 2, yPos: height / 2}]);
 
+var createHero = function() {
+  var hero = svgContainer.selectAll('hero')
+                         .data([{x: 1, y: 1, xPos: width / 2, yPos: height / 2}]);
+  hero.enter().append('circle')
+            .attr('cx', d => d.xPos)
+            .attr('cy', d => d.yPos)
+            .attr('fill', heroColor) 
+            .attr('r', heroRadius)
+            .classed('hero', true)
+            .call(drag);
+};
 
-hero.enter().append('circle')
-              .attr('cx', d => d.xPos)
-              .attr('cy', d => d.yPos)
-             .attr('fill', heroColor) 
-             .attr('r', heroRadius)
-             .classed('hero', true)
-             .call(drag);
+var startGame = function() {
+  createHero();
+  update();
+  setInterval(update, duration);
+  setInterval(collision, 10);
+  addVillians();
+};
 
-
-update();
-update();
-setInterval(update, duration);
-
-
-
-setInterval(collision, 10);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+startGame();
 
 
